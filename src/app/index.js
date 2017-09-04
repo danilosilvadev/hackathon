92b91d54c1ce8946663584cf9fad361cce4e7ff6
danilosilvadev <danilosilvaweb@hotmail.com>
2017-09-04 10:02:56 -0300

    site

--- src/app/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from './components/container';
-import './assets/images/favicon.ico';
-
-
-ReactDOM.render(<Container />, document.getElementById('root'));
